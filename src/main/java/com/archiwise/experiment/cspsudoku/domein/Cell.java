f526ab45fe94cd3d029b30600e63182a7e172e73
Nelis Boucke <nelis.boucke@archiwise.com>
2015-10-24 14:12:41 +0200

    Add implementation for getting the possible values

--- src/main/java/com/archiwise/experiment/cspsudoku/domein/Cell.java
+++ src/main/java/com/archiwise/experiment/cspsudoku/domein/Cell.java
@@ -1,8 +1,21 @@
 package com.archiwise.experiment.cspsudoku.domein;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by nelis on 24/10/15.
  */
 public class Cell {
 
+    private List<Integer> possibleValues = Arrays.asList(1,2,3,4,5,6);
+
+    public List<Integer> getPossibleValues() {
+        return possibleValues;
+    }
+
+    public int getNumberOfPossibleValues() {
+        return possibleValues.size();
+    }
+    
 }
