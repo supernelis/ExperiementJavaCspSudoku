fcdc67a51457ca29583af81cc6ff4feae5d6a166
Nelis Boucke <nelis.boucke@archiwise.com>
2015-11-05 22:06:01 +0100

    Test WhenPredefinedValueIsSet_ThenOutputChannelsAreUpdated and helper method to verify a value read

--- src/main/java/com/archiwise/experiment/cspsudoku/domein/Region.java
+++ src/main/java/com/archiwise/experiment/cspsudoku/domein/Region.java
@@ -1,6 +1,8 @@
 package com.archiwise.experiment.cspsudoku.domein;
 
+import org.jcsp.lang.AltingChannelInput;
 import org.jcsp.lang.CSProcess;
+import org.jcsp.lang.ChannelInput;
 import org.jcsp.lang.ChannelOutput;
 import org.jcsp.lang.One2OneChannel;
 
@@ -13,8 +15,16 @@ import java.util.Optional;
 public class Region implements CSProcess {
 
     private ChannelOutput<ValueAtPos> display;
-    private Optional<One2OneChannel<ValueAtPos>> leftIn = Optional.empty();
-    private Optional<One2OneChannel<ValueAtPos>> rightOut = Optional.empty();
+    private Optional<AltingChannelInput<ValueAtPos>> westIn = Optional.empty();
+    private Optional<ChannelOutput<ValueAtPos>> eastOut = Optional.empty();
+    private Optional<ChannelOutput<ValueAtPos>> westOut = Optional.empty();
+    private Optional<AltingChannelInput<ValueAtPos>> eastIn = Optional.empty();
+    private Optional<AltingChannelInput<ValueAtPos>> nordIn = Optional.empty();
+    private Optional<ChannelOutput<ValueAtPos>> nordOut = Optional.empty();
+    private Optional<ChannelOutput<ValueAtPos>> southOut = Optional.empty();
+    private Optional<AltingChannelInput<ValueAtPos>> southIn = Optional.empty();
+
+
 
     public Region(final ChannelOutput display) {
         this.display = display;
@@ -22,27 +32,63 @@ public class Region implements CSProcess {
 
     public void setPredefinedValue(final ValueAtPos valueAtPos){
         display.write(valueAtPos);
+        writeIfPresent(valueAtPos,eastOut);
+        writeIfPresent(valueAtPos,nordOut);
+        writeIfPresent(valueAtPos,southOut);
+        writeIfPresent(valueAtPos,westOut);
+    }
+
+    private void writeIfPresent(final ValueAtPos valueAtPos, final Optional<ChannelOutput<ValueAtPos>> channel) {
+        if(channel.isPresent()) channel.get().write(valueAtPos);
     }
 
     @Override
     public void run() {
-        processLeft();
+        processNeighborMessage(westIn, eastOut);
+
+
     }
 
-    private void processLeft() {
-        if(leftIn.isPresent() && leftIn.get().in().pending()){
-            ValueAtPos in = leftIn.get().in().read();
-            if(rightOut.isPresent())
-                rightOut.get().out().write(in);
+    public void setWestOut(final ChannelOutput<ValueAtPos> westOut) {
+        this.westOut = Optional.of(westOut);
+    }
+
+    public void setEastIn(final AltingChannelInput<ValueAtPos> eastIn) {
+        this.eastIn = Optional.of(eastIn);
+    }
+
+    public void setNordIn(final AltingChannelInput<ValueAtPos> nordIn) {
+        this.nordIn = Optional.of(nordIn);
+    }
+
+    public void setNordOut(final ChannelOutput<ValueAtPos> nordOut) {
+        this.nordOut = Optional.of(nordOut);
+    }
+
+    public void setSouthOut(final ChannelOutput<ValueAtPos> southOut) {
+        this.southOut = Optional.of(southOut);
+    }
+
+    public void setSouthIn(final AltingChannelInput<ValueAtPos> southIn) {
+        this.southIn = Optional.of(southIn);
+    }
+
+    private void processNeighborMessage(final Optional<AltingChannelInput<ValueAtPos>> channelIn, final Optional<ChannelOutput<ValueAtPos>> channelOut) {
+        if(channelIn.isPresent() && channelIn.get().pending()){
+            ValueAtPos valueIn = channelIn.get().read();
+
+            writeIfPresent(valueIn,channelOut);
+
         }
     }
 
-    public void setLeftIn(final One2OneChannel<ValueAtPos> leftIn) {
-        this.leftIn = Optional.of(leftIn);
+
+    public void setWestIn(final AltingChannelInput<ValueAtPos> westIn) {
+        this.westIn = Optional.of(westIn);
     }
 
-    public void setRightOut(final One2OneChannel<ValueAtPos> rightOut) {
-        this.rightOut = Optional.of(rightOut);
+    public void setEastOut(final ChannelOutput<ValueAtPos> eastOut) {
+        this.eastOut = Optional.of(eastOut);
     }
 }
 
--- src/test/java/com/archiwise/experiment/cspsudoku/domein/RegionTest.java
+++ src/test/java/com/archiwise/experiment/cspsudoku/domein/RegionTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by nelis on 02/11/15.
@@ -22,6 +23,7 @@ public class RegionTest {
     private One2OneChannel<ValueAtPos> nordOut;
     private One2OneChannel<ValueAtPos> southIn;
     private One2OneChannel<ValueAtPos> southOut;
+    private One2OneChannel<ValueAtPos> westOut;
 
     @Before
     public void setUp(){
@@ -29,7 +31,7 @@ public class RegionTest {
         westIn = Channel.one2one(new Buffer<ValueAtPos>(1));
         eastOut = Channel.one2one(new Buffer<ValueAtPos>(1));
         eastIn = Channel.one2one(new Buffer<ValueAtPos>(1));
-        westIn = Channel.one2one(new Buffer<ValueAtPos>(1));
+        westOut = Channel.one2one(new Buffer<ValueAtPos>(1));
         nordIn = Channel.one2one(new Buffer<ValueAtPos>(1));
         nordOut = Channel.one2one(new Buffer<ValueAtPos>(1));
         southIn = Channel.one2one(new Buffer<ValueAtPos>(1));
@@ -47,48 +49,76 @@ public class RegionTest {
         ValueAtPos predefinedValue = new ValueAtPos(value, row, col);
         region.setPredefinedValue(predefinedValue);
 
-        ValueAtPos valueAtPos = display.in().read();
-
-        assertEquals(predefinedValue,valueAtPos);
+        verifyChannelContainsValue(display, predefinedValue);
     }
 
     @Test
     public void WhenPredefinedValueIsSet_ThenOutputChannelsAreUpdated(){
+
+        //Given
+
         final int value=1;
         final int row=1;
         final int col=1;
 
         ValueAtPos predefinedValue = new ValueAtPos(value, row, col);
+
+        region.setWestIn(westIn.in());
+        region.setWestOut(westOut.out());
+        region.setEastIn(eastIn.in());
+        region.setEastOut(eastOut.out());
+        region.setSouthIn(southIn.in());
+        region.setSouthOut(southOut.out());
+        region.setNordIn(nordIn.in());
+        region.setNordOut(nordOut.out());
+
+        // When
         region.setPredefinedValue(predefinedValue);
 
-        ValueAtPos valueAtRight = eastOut.in().read();
+        // Then
+        verifyChannelContainsValue(eastOut, predefinedValue);
+        verifyChannelContainsValue(nordOut, predefinedValue);
+        verifyChannelContainsValue(southOut, predefinedValue);
+        verifyChannelContainsValue(westOut, predefinedValue);
+
+    }
+
+    /**
+     * Helper method to verify if a value is available on a channel
+     *
+     * The method asserts that there is a value available, reads the value and asserts that it equals the value it gets as parameter.
+     *
+     * @param channel
+     * @param predefinedValue
+     */
+    private void verifyChannelContainsValue(final One2OneChannel<ValueAtPos> channel, final ValueAtPos predefinedValue) {
+        //There is a value on the channel
+        assertTrue(channel.in().pending());
+        // Get the value
+        ValueAtPos valueAtRight = channel.in().read();
+        // Compare the value with the predefined value
         assertEquals(predefinedValue,valueAtRight);
-
-        //ValueAtPos valueAtLeft = leftOut.in().read();
-
-
     }
 
     @Test
     public void WhenValueIsReceivedLeft_ValueIshandedToRight(){
         final ValueAtPos valueSend = new ValueAtPos(5, 2, 2);
-        region.setLeftIn(westIn);
-        region.setRightOut(eastOut);
+
+        region.setWestIn(westIn.in());
+        region.setEastOut(eastOut.out());
 
         westIn.out().write(valueSend);
 
         region.run();
 
-        final ValueAtPos valueReceived = eastOut.in().read();
-
-        assertEquals(valueSend,valueReceived);
+        verifyChannelContainsValue(eastOut, valueSend);
     }
 
     @Test
     public void WhenNoRightChannelIsSet_ThenNoException(){
         final ValueAtPos valueSend = new ValueAtPos(5, 2, 2);
         westIn.out().write(valueSend);
-        region.setLeftIn(westIn);
+        region.setWestIn(westIn.in());
 
         region.run();
     }
